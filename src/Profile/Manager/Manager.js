5279ff7f3b6dddaefa474fee3ca0d38402ca4f10
Alexandr <sashakobzeww@gmail.com>
2019-09-01 20:14:39 +0300

    chr Manager Создана основа разметки

--- src/Profile/Manager/Manager.js
+++ src/Profile/Manager/Manager.js
@@ -4,7 +4,15 @@ import './Manager.css'
 class Manager extends Component {
     render() {
         return (
-            <div/>
+            <div className="Manager">
+                <span className='ManagerPhoto'/>
+                <div className="ManagerRight">
+                    <h5 className="ManagerName">Вероника Ростова</h5>
+                    <p className="ManagerInfo">Менеджер по продажам</p>
+                    <p className="ManagerStatus">Подберу для Вас самые лучшие предлжения.
+                        Мои услуги абсолютно бесплатны</p>
+                </div>
+            </div>
         );
     }
 }
