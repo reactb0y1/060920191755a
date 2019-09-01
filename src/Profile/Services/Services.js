515793ed34156e53f515c2542f77a5c6c366add7
Alexandr <sashakobzeww@gmail.com>
2019-09-01 21:44:07 +0300

    chr Services Заданы базовая разметка и стили

--- src/Profile/Services/Services.js
+++ src/Profile/Services/Services.js
@@ -4,7 +4,32 @@ import './Services.css'
 class Services extends Component {
     render() {
         return (
-            <div/>
+            <div className="Services">
+                <table className='ServicesTable'>
+                    <tbody>
+                        <tr>
+                            <td/>
+                            <td>Услуги</td>
+                        </tr>
+                        <tr>
+                            <td>Ручное бронирование</td>
+                            <td>11</td>
+                        </tr>
+                        <tr>
+                            <td>Пакетные туры</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>Отели</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>Всего</td>
+                            <td>15</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
