f67dfe0351abcf5a2c72f971eaa33fa52cf61221
Alexandr <sashakobzeww@gmail.com>
2019-09-02 15:44:11 +0300

    chr CommentsHead Задана базовая разметка

--- src/Comments/CommentsHead/CommentsHead.js
+++ src/Comments/CommentsHead/CommentsHead.js
@@ -4,7 +4,28 @@ import './CommentsHead.css'
 class CommentsHead extends Component {
     render() {
         return (
-            <div/>
+            <div className="CommentsHead">
+
+                <div className="reviews">
+                    <h3 className='reviewsLast'>Последние отзывы</h3>
+                    <h3 className='reviewsAll'>Все отзывы</h3>
+                </div>
+
+                <div className="CommentsHeadInfo">
+
+                    <div className="likes">
+                        <span className='likesIco'/>
+                        <p  className="likesText">131</p>
+                    </div>
+
+                    <div className="comments">
+                        <span className='commentsIco'/>
+                        <p className="commentsText">14</p>
+                    </div>
+
+                </div>
+
+            </div>
         );
     }
 }
