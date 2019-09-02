acde58e2a4b54e499bd8b1ac017d734a259354b3
Alexandr <sashakobzeww@gmail.com>
2019-09-02 20:17:04 +0300

    chr Comment Готов в первом приближении

--- src/Comments/Comment/Comment.js
+++ src/Comments/Comment/Comment.js
@@ -4,7 +4,21 @@ import './Comment.css'
 class Comment extends Component {
     render() {
         return (
-            <div/>
+            <div className="Comment">
+
+                <h5 className="CommentName">Самуил</h5>
+
+                <span className="CommentDate">13 октября 2011</span>
+
+                <div className="CommentTextContainer">
+                    <p className="CommentText">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+
+            </div>
         );
     }
 }
