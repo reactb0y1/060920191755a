db3ebe26ae580d1d5bd1b40fc3d7eeda4252a947
Alexandr <sashakobzeww@gmail.com>
2019-09-04 13:17:52 +0300

    chr Создали продакшн версию

---
+++ build/precache-manifest.4a699a8038905e0426d33add23f7e82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc9de5b16ed61e248e95ca9776baad3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fce675d42055e52db17",
+    "url": "/static/css/main.16f6ab9a.chunk.css"
+  },
+  {
+    "revision": "124fee97c555847443da",
+    "url": "/static/js/2.2ceeae82.chunk.js"
+  },
+  {
+    "revision": "4fce675d42055e52db17",
+    "url": "/static/js/main.a666f6a1.chunk.js"
+  },
+  {
+    "revision": "519b7fd0c2fb22240b1c",
+    "url": "/static/js/runtime~main.cc2a4a22.js"
+  },
+  {
+    "revision": "2f385f4c28f8578292911e8760bc3185",
+    "url": "/static/media/ManagerPhoto.2f385f4c.png"
+  }
+]);
